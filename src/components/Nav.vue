4bee4ef5bac336cb4b175e00e5059496dd5ee5e1
Melissa Ben <melissabenmeziane01@gmail.com>
2022-03-11 11:38:01 +0100

    CleanUp

--- src/components/Nav.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div>
-        <b-nav>
-            <b-nav-item>Link</b-nav-item>
-            <b-nav-item>Another Link</b-nav-item>
-            <b-nav-item disabled>Disabled</b-nav-item>
-        </b-nav>
-    </div>
-</template>
