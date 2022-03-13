1dc1d4e2494323fdd069058dd5829b05fee05110
takwa <blgeyatatou@gmail.com>
2022-03-13 21:28:35 +0100

    add changes

--- src/pages/Show.vue
+++ src/pages/Show.vue
@@ -1,95 +1,155 @@
 <template>
   <div class="">
-   <section class="position-relative gen-section-padding-3">
-   <div class="tv-single-background">
-            <img src="https://picsum.photos/60/60" alt="stream-lab-image">
-        </div>
-   <div class="container">
-    <div class="col-lg-12">
-  <div class="gen-tv-show-wrapper style-1">
-     <div class="gen-tv-show-top">
-     <div class="row">
-      <div class="col-lg-6">
-            <div class="gentech-tv-show-img-holder">
-                <img  class="show-img" src="https://picsum.photos/600/400" alt="stream-lab-image">
-            </div>
-        </div>
-            <div class="col-lg-6 align-self-center">
-            <div class="gen-single-tv-show-info">
-                <h2 class="gen-title">Love In 21st</h2>
-                <div class="gen-single-meta-holder">
-                    <ul>
-                        <li>2 Seasons</li>
-                        <li>9 Episodes</li>
-                        <li>2015 to 2016</li>
-                        <li>
-                            <a href="#"><span>Action</span></a>
-                        </li>
-                        <li>
-                            <i class="fas fa-eye">
-                            </i>
-                            <span>5.5K Views</span>
-                        </li>
-                    </ul>
+    <section class="position-relative gen-section-padding-3">
+      <div class="tv-single-background">
+        <img src="https://picsum.photos/60/60" alt="stream-lab-image" />
+      </div>
+      <div class="container">
+        <div class="col-lg-12">
+          <div class="gen-tv-show-wrapper style-1">
+            <div class="gen-tv-show-top">
+              <div class="row">
+                <div class="col-lg-6">
+                  <div class="gentech-tv-show-img-holder">
+                    <img
+                      v-if="show"
+                      class="showe"
+                      :src="show.image.medium"
+                      alt="stream-lab-image"
+                    />
+                  </div>
                 </div>
-                <p>Streamlab is a long established fact that a reader will be distracted by the
-                    readable content of a page when Streamlab at its layout. The point of using
-                    Lorem Streamlab is that it has a more-or-less normal distribution of
-                    Streamlab
-                    as opposed Streamlab.</p>
-                <div class="gen-socail-share mt-0">
-                    <h4 class="align-self-center">Social Share :</h4>
-                    <ul class="social-inner">
-                        <li><a href="#" class="facebook"><i class="fab fa-facebook-f"></i></a>
+                <div class="col-lg-6 align-self-center">
+                  <div class="gen-single-tv-show-info">
+                    <h2 v-if="show" class="gen-title">{{ show.name }}</h2>
+                    <div class="gen-single-meta-holder">
+                      <ul>
+                        <li v-if="show">Duration: {{ show.runtime }}</li>
+                        <li v-if="show">Language: {{ show.language }}</li>
+                        <li v-if="show">Air date: {{ show.premiered }}</li>
+                        <li v-if="show">Type: {{ show.type }}</li>
+                        <li v-if="show">
+                          <i class="fa fa-star"></i>
+                          {{ show.rating.average }}
                         </li>
-                        <li><a href="#" class="facebook"><i class="fab fa-instagram"></i></a>
+                      </ul>
+                      <ul>
+                        <li v-if="show">
+                          <h6>Official site:</h6>
+                          <a class="site" :href="show.officialSite">
+                            {{ show.officialSite }}</a
+                          >
                         </li>
-                        <li><a href="#" class="facebook"><i class="fab fa-twitter"></i></a></li>
-                    </ul>
+                      </ul>
+                    </div>
+                    <p v-if="show" v-html="show.summary"></p>
+                  </div>
                 </div>
-
+              </div>
             </div>
+          </div>
+          <div class="gen-season-holder">
+            <ul class="nav">
+              <li v-for="season in saisons" :key="season.id" class="nav-item">
+                <a
+                  v-if="season"
+                  class="nav-link"
+                  data-toggle="tab"
+                  :href="'#season_' + season.number"
+                  >Season{{ season.number }}</a
+                >
+              </li>
+            </ul>
+            <div class="tab-content">
+              <div
+                v-for="ep in episodes"
+                :key="ep.id"
+                :id="'season_' + ep.season"
+                class="tab-pane epi-season"
+              >
+                <div
+                  class="owl-carousel owl-loaded owl-drag"
+                  data-dots="false"
+                  data-nav="true"
+                  data-desk_num="4"
+                  data-lap_num="3"
+                  data-tab_num="2"
+                  data-mob_num="1"
+                  data-mob_sm="1"
+                  data-autoplay="false"
+                  data-loop="false"
+                  data-margin="30"
+                >
+                  <div class="item all-ep">
+                    <div
+                      v-if="ep.season == 1"
+                      class="gen-episode-contain episodee"
+                    >
+                      <div class="gen-episode-img">
+                        <img
+                          :src="ep.image.medium"
+                          class="img-ep"
+                          alt="stream-lab-image"
+                        />
+                        <div class="gen-movie-action">
+                          <a href="tv-shows-home.html" class="gen-button">
+                            <i class="fa fa-play"></i>
+                          </a>
+                        </div>
+                      </div>
+                      <div class="gen-info-contain">
+                        <div class="gen-episode-info">
+                          <h3>
+                            {{ ep.name }} <span>-</span>
+                            <a href="#">
+                              Episode: {{ ep.number }} season {{ ep.season }}
+                            </a>
+                          </h3>
+                        </div>
+                        <div class="gen-single-meta-holder">
+                          <ul>
+                            <li class="run-time">{{ ep.runtime }}min</li>
+
+                            <li class="release-date">
+                              {{ ep.airdate }}
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-     </div>
-    </div>
-  </div>
-<div class="gen-season-holder">
- <ul class="nav">
-  <li class="nav-item">
-  <a class="nav-link active show" data-toggle="tab" href="#season_0">Season 1</a></li>
-  <li class="nav-item"> <a class="nav-link" data-toggle="tab" href="#season_1">Season 2</a></li>
-  </ul>
-   <div class="tab-content"></div>
-</div>
-</div>
-</div>
-           
-   </section>
-   
-    <div class="container m-5">
-     <h1>show :</h1>
-      <p>{{ show }}</p>
-    </div> 
-
-    <div>
-        <form @submit="submitComment">
-            <input v-model="form.username" name="username" placeholder="Username"> <br>
-            <textarea class="mt-2" v-model="form.content" name="content" placeholder="Content"></textarea> <br>
-            <button class="btn btn-outline-success" type="submit">Commenter</button>
-        </form>
-
-    </div>
-
-    <h1>seasons :</h1>
-
-    <div v-for='season in saisons' :key='season.id'>
-         <p>{{season.name}}</p>
-    </div>
-
+      </div>
+    </section>
+    <!-- <div>
+      <form @submit="submitComment">
+        <input v-model="form.username" name="username" placeholder="Username" />
+        <br />
+        <textarea
+          class="mt-2"
+          v-model="form.content"
+          name="content"
+          placeholder="Content"
+        ></textarea>
+        <br />
+        <button class="btn btn-outline-success" type="submit">Commenter</button>
+      </form>
+    </div> -->
     <h1>episodes :</h1>
-    <div v-for='ep in episodes' :key='ep.id'>
-         <p>{{ep}}</p>
-         <a href="" @click.prevent="$router.push({ name: 'single_episode', params: { id: ep.id } })" class="btn btn-primary">Link</a>
+    <div v-for="ep in episodes" :key="ep.id">
+      <p>{{ ep }}</p>
+      <a
+        href=""
+        @click.prevent="
+          $router.push({ name: 'single_episode', params: { id: ep.id } })
+        "
+        class="btn btn-primary"
+        >Details</a
+      >
     </div>
   </div>
 </template>
@@ -98,23 +158,21 @@
 import Search from "../components/Search.vue";
 
 export default {
-  name: 'Show',
-  components: { Search },   
+  name: "Show",
+  components: { Search },
 
-  data () {
+  data() {
     return {
-        form: {
-            username: '',
-            content: '',
-        },
-        id: null,
-    //   saisons: null,
-    //   episodes: null,
-
-    }
+      form: {
+        username: "",
+        content: "",
+      },
+      id: null,
+      //   saisons: null,
+      //   episodes: null,
+    };
   },
   computed: {
-
     show() {
       return this.$store.state.show;
     },
@@ -129,34 +187,46 @@ export default {
   },
   methods: {
     getshow() {
-        this.$store.dispatch("getSingleShow", this.id)
-        this.$store.dispatch("getSingleShowSaisons", this.id)
-        this.$store.dispatch("getSingleShowEpisodes", this.id)
+      this.$store.dispatch("getSingleShow", this.id);
+      this.$store.dispatch("getSingleShowSaisons", this.id);
+      this.$store.dispatch("getSingleShowEpisodes", this.id);
     },
-    submitComment(e){
-        e.preventDefault();
-        console.log(this.form)
-        this.$store.dispatch("addComment", this.id, this.form)
-    }
+    submitComment(e) {
+      e.preventDefault();
+      console.log(this.form);
+      console.log(this.saisons);
+      this.$store.dispatch("addComment", this.id, this.form);
+    },
   },
   mounted() {
-      this.id = this.$route.params.id
-      this.getshow()
-  }
-}
+    this.id = this.$route.params.id;
+    this.getshow();
+  },
+};
 </script>
 <style scoped>
-.container {
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
+.showe {
+  width: 100%;
+  height: 400px;
 }
-  .gentech-tv-show-img-holder {
-        margin: 0 0 30px;
-    }
-    .show-img{
-      height:auto;
-      max-width:100%;
-    }
-    
+.site {
+  --white-color: red;
+  margin-left: 7px;
+  font-size: 22px;
+}
+.img-medium {
+  height: 290px;
+  width: 370px;
+}
+.episodee {
+  width: 300px;
+  height: 223px;
+}
+.all-ep {
+  display: inline-table;
+  margin: 20px;
+}
+.epi-season {
+  display: inline-table !important;
+}
 </style>
