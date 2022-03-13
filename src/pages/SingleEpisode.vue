46e65cd1bb422ef0cf5a3501a378aa6d7549068f
takwa <blgeyatatou@gmail.com>
2022-03-13 16:24:44 +0100

    add changes

--- src/pages/SingleEpisode.vue
+++ src/pages/SingleEpisode.vue
@@ -8,7 +8,51 @@
               <div class="row">
                 <div class="col-lg-12">
                   <div class="gen-video-holder">
-                    <img v-if="episode" :src="episode.image.original" />
+                    <!-- <img v-if="episode" :src="episode.image.original" /> -->
+                    <div v-if="episode" class="gen-front-image">
+                      <img
+                        :src="episode.image.original"
+                        alt="owl-carousel-banner-image"
+                      />
+                      <a
+                        href="#"
+                        class="playBut popup-youtube popup-vimeo popup-gmaps"
+                      >
+                        <svg
+                          version="1.1"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="213.7px"
+                          height="213.7px"
+                          viewBox="0 0 213.7 213.7"
+                          enable-background="new 0 0 213.7 213.7"
+                          xml:space="preserve"
+                        >
+                          <polygon
+                            class="triangle"
+                            id="XMLID_17_"
+                            fill="none"
+                            stroke-width="7"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-miterlimit="10"
+                            points="73.5,62.5 148.5,105.8 73.5,149.1 "
+                          ></polygon>
+                          <circle
+                            class="circle"
+                            id="XMLID_18_"
+                            fill="none"
+                            stroke-width="7"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-miterlimit="10"
+                            cx="106.8"
+                            cy="106.8"
+                            r="103.3"
+                          ></circle>
+                        </svg>
+                        <span>Watch Trailer</span>
+                      </a>
+                    </div>
                     <iframe
                       v-else
                       width="100%"
@@ -34,26 +78,22 @@
                           <li v-if="episode">
                             <span>Season : {{ episode.season }}</span>
                           </li>
+                          <li v-if="episode">
+                            <span>Episode : {{ episode.number }}</span>
+                          </li>
+                          <li v-if="episode">
+                            <span>Air date : {{ episode.airdate }}</span>
+                          </li>
                         </ul>
                       </div>
                       <p v-if="episode" v-html="episode.summary"></p>
                       <p v-else>there is no summary for this episode</p>
                       <div class="gen-socail-share">
-                        <h4 class="align-self-center">Social Share :</h4>
+                        <h4 class="align-self-center">Back :</h4>
                         <ul class="social-inner">
                           <li>
-                            <a href="#" class="facebook"
-                              ><i class="fab fa-facebook-f"></i
-                            ></a>
-                          </li>
-                          <li>
-                            <a href="#" class="facebook"
-                              ><i class="fab fa-instagram"></i
-                            ></a>
-                          </li>
-                          <li>
-                            <a href="#" class="facebook"
-                              ><i class="fab fa-twitter"></i
+                            <a href="/" class="facebook"
+                              ><i class="fa fa-arrow-left"></i
                             ></a>
                           </li>
                         </ul>
@@ -94,4 +134,11 @@ export default {
 };
 </script>
 <style scoped>
+img {
+  position: relative;
+  left: 2%;
+  margin-top: 20px;
+  height: 550px;
+  width: 1500px;
+}
 </style>
